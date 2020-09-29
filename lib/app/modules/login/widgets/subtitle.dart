926b8a9024af8e6911ed991d1f26e358725b0729
Uriel <ubonano@gmail.com>
2020-09-28 23:26:35 -0300

    subtitle refactor

--- lib/app/modules/login/widgets/subtitle.dart
+++ lib/app/modules/login/widgets/subtitle.dart
@@ -3,19 +3,19 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:habitat_ft_user/app/config/colors.dart';
 
 class SubTitle extends StatelessWidget {
+
+  final String text;
+  
   const SubTitle({
-    Key key,
+    Key key, @required this.text,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 12.0),
-      child: Text(
-        'Inicio de sesion',
-        style: GoogleFonts.spartan(
-            color: HColors.GRIS_50, fontWeight: FontWeight.w600),
-      ),
+    return Text(
+      text,
+      style: GoogleFonts.spartan(
+          color: HColors.GRIS_50, fontWeight: FontWeight.w600),
     );
   }
 }
