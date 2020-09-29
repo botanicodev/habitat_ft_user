712d46cb893e51d9be285a94f721d6b1bdccea98
Uriel <ubonano@gmail.com>
2020-09-28 22:26:57 -0300

    Add welcome title widget

---
+++ lib/app/modules/login/widgets/welcome_title.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:habitat_ft_user/app/config/colors.dart';
+
+class WelcomeTitle extends StatelessWidget {
+  const WelcomeTitle({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '¡Bienvenido!',
+      style: GoogleFonts.spartan(
+          color: HColors.CELESTE_HABITAT,
+          fontSize: 30,
+          fontWeight: FontWeight.w300),
+    );
+  }
+}
