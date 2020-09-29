befdcfd58ae69a32824a326c4ea2711da70a4706
Uriel <ubonano@gmail.com>
2020-09-28 23:27:21 -0300

    login view

--- lib/app/modules/login/login_view.dart
+++ lib/app/modules/login/login_view.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart'; 
+import 'package:get/get.dart';
 import 'package:habitat_ft_user/app/modules/login/login_controller.dart';
+
+import 'widgets/background.dart';
+import 'widgets/email_text_field.dart';
+import 'widgets/logo.dart';
+import 'widgets/login_button.dart';
+import 'widgets/password_text_field.dart';
+import 'widgets/subtitle.dart';
+import 'widgets/htitle.dart';
 
 class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LoginView is working', 
-          style: TextStyle(fontSize:20),
-        ),
+      body: Background(
+        children: [
+          Logo(),
+          HTitle(text: '¡Bienvenido!'),
+          SubTitle(text: 'Inicio de sesion'),
+          EmailTextField(),
+          PasswordTextField(),
+          LoginButton(),
+        ],
       ),
     );
   }
 }
-  
