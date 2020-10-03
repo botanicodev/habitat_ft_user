ec96d0ddef2b600388a7d999e627b91c36b27e94
Uriel <ubonano@gmail.com>
2020-10-03 17:37:05 -0300

    User drawer

---
+++ lib/app/modules/home/widgets/drawer_body.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:habitat_ft_user/app/config/colors.dart';
+
+import 'user_tile.dart';
+import 'sign_out_button.dart';
+
+class DrawerBody extends StatelessWidget {
+  const DrawerBody({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: HColors.CELESTE_OSCURO,
+        child: Column(
+          children: [
+            UserTile(),
+            Spacer(),
+            SignOutButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
