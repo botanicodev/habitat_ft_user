c6e75d8c09db2e06bbaf03370f1ced21b7956ad5
Uriel <ubonano@gmail.com>
2020-10-04 14:26:36 -0300

    dialog confirm sigout

--- lib/app/modules/home/widgets/sign_out_tile.dart
+++ lib/app/modules/home/widgets/sign_out_tile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:habitat_ft_user/app/config/colors.dart';
+import 'package:habitat_ft_user/app/config/styles.dart';
 import 'package:habitat_ft_user/app/controllers/auth_controller.dart';
 
 class SignOutTile extends StatelessWidget {
@@ -18,24 +18,71 @@ class SignOutTile extends StatelessWidget {
         color: HColors.CELESTE_HABITAT,
         child: Center(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal:20),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: ListTile(
               leading: Icon(
                 Icons.logout,
                 size: 35,
                 color: HColors.BLANCO,
               ),
-              title: Text(
-                'Cerrar sesion',
-                style: GoogleFonts.spartan(
-                  color: HColors.BLANCO,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              onTap: Get.find<AuthController>().signOut,
+              title: Text('Cerrar sesion', style: HStyles.TITULO_3_BLANCO),
+              onTap: () {
+                Get.dialog(SignOutDialog());
+              },
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignOutDialog extends StatelessWidget {
+  const SignOutDialog({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: HColors.GRIS_01,
+      content: Container(
+        width: 274,
+        height: 160,
+        // decoration: BoxDecoration(
+        //   shape: BoxShape.rectangle,
+        //   borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        // ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              '¿Desea cerrar sesion?',
+              style: HStyles.TITULO_3_CELESTE,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  iconSize: 60,
+                  icon: Icon(
+                    Icons.highlight_remove_rounded,
+                    color: HColors.CANCELAR,
+                  ),
+                  onPressed: () => Get.back(),
+                ),
+                IconButton(
+                  iconSize: 60,
+                  icon: Icon(
+                    Icons.check_circle_outline_rounded,
+                    color: HColors.CONFIRMAR,
+                    // size: 35,
+                  ),
+                  onPressed: Get.find<AuthController>().signOut,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
