0a4f9e40ec9f595fc49097014c9015a2ea7b55ea
Uriel <ubonano@gmail.com>
2020-10-05 13:33:24 -0300

    refactor WorkshopTile widget

--- lib/app/modules/home/home_view.dart
+++ lib/app/modules/home/home_view.dart
@@ -2,21 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:habitat_ft_user/app/config/assets.dart';
 import 'package:habitat_ft_user/app/config/colors.dart';
+import 'package:habitat_ft_user/app/models/workshop_model.dart';
 import 'package:habitat_ft_user/app/modules/home/home_controller.dart';
 import 'package:habitat_ft_user/app/widgets/hdrawer.dart';
 
+import 'widgets/workshops_pending.dart';
 import 'widgets/workshops_title.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: HColors.GRIS_01,
       appBar: buildAppBar(),
       drawer: HDrawer(),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           WorkshopsTitle(),
+          WorkshopsPending(
+            list: [
+              Workshop(title:'Titulo taller'),
+              Workshop(title:'Titulo taller'),
+              Workshop(title:'Titulo taller'),
+              Workshop(title:'Titulo taller'),
+              Workshop(title:'Titulo taller'),
+            ],
+          ),
         ],
       ),
     );
--- lib/app/modules/home/widgets/workshop_tile.dart
+++ lib/app/modules/home/widgets/workshop_tile.dart
@@ -5,35 +5,47 @@ import 'package:habitat_ft_user/app/models/workshop_model.dart';
 import 'package:habitat_ft_user/app/widgets/background_tile.dart';
 
 class WorkshopTile extends StatelessWidget {
-
   final Workshop workshop;
 
   const WorkshopTile({
-    Key key, @required this.workshop,
+    Key key,
+    @required this.workshop,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BackgroundTile(
       child: ListTile(
-        title: Text(
-          workshop.title,
-          style: HStyles.TITULO_3_CELESTE,
-        ),
-        leading: ClipOval(
-          child: Container(
-            height: 40,
-            width: 40,
-            color: Colors.pink,
-          ),
-        ),
-        trailing: Icon(
-          Icons.play_circle_fill_rounded,
-          color: HColors.CELESTE_HABITAT,
-          size: 40,
-        ),
+        title: buildTitle(),
+        leading: buildLeading(),
+        trailing: buildTrailing(),
         onTap: () {},
       ),
+    );
+  }
+
+  Widget buildTitle() {
+    return Text(
+      workshop.title,
+      style: HStyles.TITULO_3_CELESTE,
+    );
+  }
+
+  Widget buildLeading() {
+    return ClipOval(
+      child: Container(
+        height: 40,
+        width: 40,
+        color: Colors.pink,
+      ),
+    );
+  }
+
+  Widget buildTrailing() {
+    return Icon(
+      Icons.play_circle_fill_rounded,
+      color: HColors.CELESTE_HABITAT,
+      size: 40,
     );
   }
 }
