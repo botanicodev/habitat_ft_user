f48decd6642ee7ab69313714f9bdbc8f2437e8d1
Uriel <ubonano@gmail.com>
2020-10-05 13:37:28 -0300

    modified workshop model

--- lib/app/models/workshop_model.dart
+++ lib/app/models/workshop_model.dart
@@ -1,18 +1,21 @@
 class Workshop {
   String id;
   String title;
+  String status;
 
   Workshop({this.title});
 
   Workshop.fromJson(Map<String, dynamic> json) {
     id = json['workshop_id'];
     title = json['title'];
+    status = json['status'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = Map<String, dynamic>();
-    data['title'] = this.title;
     data['workshop_id'] = this.id;
+    data['title'] = this.title;
+    data['status'] = this.status;
     return data;
   }
 }
