1747c0fed5c4d6991611395c166426e00055a7eb
Uriel <ubonano@gmail.com>
2020-10-08 01:11:35 -0300

    fix web config

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: "Habitat",
+      title: "Habitat Argentina",
       debugShowCheckedModeBanner: false,
       initialRoute: AppPages.INITIAL,
       initialBinding: SplashScreenBinding(),
