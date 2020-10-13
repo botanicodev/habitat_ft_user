08228418b7070b41530fb5d7b94ee26fb3e63de0
Uriel <ubonano@gmail.com>
2020-10-12 21:35:37 -0300

    workshop view

--- lib/app/modules/workshop/workshop_view.dart
+++ lib/app/modules/workshop/workshop_view.dart
@@ -3,16 +3,22 @@ import 'package:get/get.dart';
 import 'package:habitat_ft_user/app/modules/workshop/workshop_controller.dart';
 import 'package:habitat_ft_user/app/utils/build_widget.dart';
 
+import 'widgets/component_page_view.dart';
+
 class WorkshopView extends GetView<WorkshopController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BuildWidget.buildAppBar(),
-      body: Center(
-        child: Text(
-          'WorkshopDisplayView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Obx(
+        () {
+          return PageView(
+            controller: controller.pageController,
+            children: controller.components
+                .map((component) => ComponentPageView(component))
+                .toList(),
+          );
+        },
       ),
     );
   }
