8eb849f52bcf79cddbdd5701fb4858566bd0a9d9
Uriel <ubonano@gmail.com>
2020-10-13 17:28:45 -0300

    refactor navigation buttons

--- lib/app/modules/workshop/widgets/component_page_view.dart
+++ lib/app/modules/workshop/widgets/component_page_view.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:habitat_ft_user/app/data/models/component_model.dart';
-import 'package:habitat_ft_user/app/modules/workshop/widgets/page_button.dart';
 
-import '../workshop_controller.dart';
 import 'background.dart';
 import 'header.dart';
+import 'navigation_buttons.dart';
 
 class ComponentPageView extends StatelessWidget {
-  final WorkshopController workshopController = Get.find();
   final Component component;
 
   ComponentPageView(
@@ -24,29 +21,9 @@ class ComponentPageView extends StatelessWidget {
           Header(component: component),
           // VER DE METER UN CONTROLLER PARA CADA PAGE,
           Spacer(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              buildPreviusButton(),
-              buildNextButton(),
-            ],
-          ),
+          NavigationButtons(),
         ],
       ),
-    );
-  }
-
-  Widget buildPreviusButton() {
-    return PageButton(
-      onPressed: workshopController.previusPage,
-      icon: Icons.arrow_back_ios,
-    );
-  }
-
-  Widget buildNextButton() {
-    return PageButton(
-      onPressed: workshopController.nextPage,
-      icon: Icons.arrow_forward_ios,
     );
   }
 }
