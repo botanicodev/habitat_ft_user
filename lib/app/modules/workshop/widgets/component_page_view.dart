e324d0666cbe561dbb8c7a7ba6867e0d4fef1636
Uriel <ubonano@gmail.com>
2020-10-14 11:56:25 -0300

    body page

--- lib/app/modules/workshop/widgets/component_page_view.dart
+++ lib/app/modules/workshop/widgets/component_page_view.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:habitat_ft_user/app/data/models/component_model.dart';
+import 'package:habitat_ft_user/app/modules/workshop/widgets/dowload_button.dart';
+import 'package:habitat_ft_user/app/utils/enums.dart';
 
 import 'background.dart';
 import 'header.dart';
@@ -19,11 +22,47 @@ class ComponentPageView extends StatelessWidget {
       child: Column(
         children: [
           Header(component),
-          // VER DE METER UN CONTROLLER PARA CADA PAGE,
+          Spacer(),
+          Body(component),
           Spacer(),
           NavigationButtons(),
         ],
       ),
     );
   }
+}
+
+class Body extends StatelessWidget {
+  final Component component;
+  const Body(
+    this.component, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    switch (component.mediaType) {
+      case MediaType.video:
+        return buildVideoBody();
+      case MediaType.image:
+        return buildImageBody();
+      case MediaType.file:
+        return buildFileBody();
+      case MediaType.audio:
+        return buildAudioBody();
+      default:
+        return Container();
+    }
+  }
+
+  Widget buildVideoBody() => Container();
+
+  Widget buildImageBody() => Image.network(component.url);
+
+  Widget buildFileBody() => Padding(
+        padding: EdgeInsets.symmetric(horizontal: 88),
+        child: DownloadButton(component),
+      );
+
+  Widget buildAudioBody() => Container();
 }
