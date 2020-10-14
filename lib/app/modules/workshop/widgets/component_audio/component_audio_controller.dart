c8160657c8f4ac1d7659536fce04df6854ed8c89
Uriel <ubonano@gmail.com>
2020-10-14 18:48:19 -0300

    Component audio

--- lib/app/modules/workshop/widgets/component_audio/component_audio_controller.dart
+++ lib/app/modules/workshop/widgets/component_audio/component_audio_controller.dart
@@ -1,28 +1,43 @@
 import 'package:get/get.dart';
 import 'package:habitat_ft_user/app/data/models/component_model.dart';
+import 'package:habitat_ft_user/app/utils/helper.dart';
 import 'package:video_player/video_player.dart';
 
 class ComponentAudioController extends GetxController {
   Rx<Component> _component = Rx<Component>();
 
-  Rx<VideoPlayerController> _audioPlayerController;
+  Rx<VideoPlayerController> _videoPlayerController;
+  RxBool _isPlaying = false.obs;
+  RxString _duration = ''.obs;
 
-  get component => this._component.value;
-
-  get audioPlayerController => this._audioPlayerController.value;
+  Component get component => this._component.value;
+  VideoPlayerController get videoPlayerController =>
+      this._videoPlayerController.value;
+  bool get isPlaying => this._isPlaying.value;
+  String get duration => this._duration.value;
 
   @override
   void onClose() {
-    audioPlayerController.dispose();
+    videoPlayerController.dispose();
   }
 
   void init({Component component}) async {
     _component.value = component;
 
-    _audioPlayerController = VideoPlayerController.network(component.url).obs;
+    _videoPlayerController = VideoPlayerController.network(component.url).obs;
 
-    await audioPlayerController.initialize();
+    await videoPlayerController.initialize();
 
-    audioPlayerController.setLooping(true);
+    _duration.value = Helper.durationToMinutesAndSeconds(
+        videoPlayerController.value.duration);
+
+    videoPlayerController.setLooping(true);
+    _videoPlayerController.value.addListener(() {
+      _isPlaying.value = videoPlayerController.value.isPlaying;
+    });
   }
+
+  void play() => videoPlayerController.play();
+
+  void pause() => videoPlayerController.pause();
 }
