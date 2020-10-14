6914c94f16cc3e9e5c82b00e58666a6de69956dd
Uriel <ubonano@gmail.com>
2020-10-14 11:56:38 -0300

    add download button

---
+++ lib/app/modules/workshop/widgets/dowload_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:habitat_ft_user/app/data/models/component_model.dart';
+import 'package:habitat_ft_user/app/utils/config/colors.dart';
+import 'package:habitat_ft_user/app/utils/config/styles.dart';
+import 'package:habitat_ft_user/app/utils/helper.dart';
+
+class DownloadButton extends StatelessWidget {
+  final Component component;
+
+  const DownloadButton(this.component, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ButtonTheme(
+      buttonColor: CustomerColors.CELESTE_HABITAT,
+      height: 55,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: RaisedButton(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'Descargar',
+              style: CustomerStyles.SUB_TITULO_1
+                  .copyWith(color: CustomerColors.BLANCO),
+            ),
+            Icon(
+              Icons.cloud_download,
+              color: CustomerColors.BLANCO,
+            ),
+          ],
+        ),
+        onPressed: () => Helper.launchURL(component.url),
+      ),
+    );
+  }
+}
