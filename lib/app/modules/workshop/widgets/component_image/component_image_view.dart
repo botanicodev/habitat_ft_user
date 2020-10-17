efb2cf4fbafd827cbe504d912d4e49a6a8485e77
Uriel <ubonano@gmail.com>
2020-10-17 17:37:08 -0300

    refact

--- lib/app/modules/workshop/widgets/component_image/component_image_view.dart
+++ lib/app/modules/workshop/widgets/component_image/component_image_view.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:get/state_manager.dart';
 import 'package:habitat_ft_user/app/data/models/component_model.dart';
 import 'package:habitat_ft_user/app/modules/workshop/widgets/component_background.dart';
 import 'package:habitat_ft_user/app/modules/workshop/widgets/component_header.dart';
 import 'package:habitat_ft_user/app/utils/config/assets.dart';
 
-import 'component_image_controller.dart';
-
-class ComponentImageView extends GetWidget<ComponentImageController> {
-  ComponentImageView(Component component) {
-    controller.init(component: component);
-  }
+class ComponentImageView extends StatelessWidget {
+  final Component component;
+  ComponentImageView(this.component);
 
   @override
   Widget build(BuildContext context) {
     return ComponentBackground(
-      header: ComponentHeader(controller.component),
-      body: FadeInImage.assetNetwork(
-        placeholder: CustomerAssets.LOADER,
-        image: controller.component.url,
-        imageErrorBuilder: imageErrorBuilder,
-      ),
+      header: header,
+      body: body,
     );
   }
+
+  Widget get header => ComponentHeader(component);
+  Widget get body => FadeInImage.assetNetwork(
+        placeholder: placeholder,
+        image: component.url,
+        imageErrorBuilder: imageErrorBuilder,
+      );
+
+  String get placeholder => CustomerAssets.LOADER;
 
   Widget imageErrorBuilder(context, object, stacktrace) =>
       Text('No se puede cargar la imagen en este momento :(');
--- lib/app/modules/workshop/workshop_binding.dart
+++ lib/app/modules/workshop/workshop_binding.dart
@@ -1,7 +1,6 @@
 import 'package:get/get.dart';
 
 import 'workshop_controller.dart';
-import 'widgets/component_image/component_image_controller.dart';
 import 'widgets/component_file/widgets/download_button/download_button_controller.dart';
 import 'widgets/component_audio/widgets/audio_player/audio_player_controller.dart';
 import 'widgets/component_video/widgets/video_player/video_player_controller.dart';
@@ -10,7 +9,6 @@ class WorkshopBinding extends Bindings {
   @override
   void dependencies() {
     Get.put<WorkshopController>(WorkshopController());
-    Get.create(() => ComponentImageController());
     Get.create(() => DownloadButtonController());
     Get.create(() => AudioPlayerController());
     Get.create(() => VideoPlayerController());
