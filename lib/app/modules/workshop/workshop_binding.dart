7e4eb0e2817ef7dc8145621d0b44e0ccc2030e8f
Uriel <ubonano@gmail.com>
2020-10-17 16:53:49 -0300

    rename downloadbutton and refact

--- lib/app/modules/workshop/workshop_binding.dart
+++ lib/app/modules/workshop/workshop_binding.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 
 import 'workshop_controller.dart';
 import 'widgets/component_image/component_image_controller.dart';
-import 'widgets/component_file/widgets/dowload_button_controller.dart';
+import 'widgets/component_file/widgets/download_button_controller.dart';
 import 'widgets/component_audio/widgets/audio_player/audio_player_controller.dart';
 import 'widgets/component_video/widgets/video_player/video_player_controller.dart';
 
