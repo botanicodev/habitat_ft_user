2bbe733dbc27d34d6fe882f7d0ac38093c852f6e
Uriel <ubonano@gmail.com>
2020-10-16 23:52:53 -0300

    refactor

--- lib/app/modules/workshop/widgets/component_audio/widgets/audio_player/widgets/audio_player_button.dart
+++ lib/app/modules/workshop/widgets/component_audio/widgets/audio_player/widgets/audio_player_button.dart
@@ -24,8 +24,6 @@ class AudioPlayerButton extends StatelessWidget {
         return buildPauseButton();
       case PlayerStatus.paused:
         return buildPlayButton();
-      case PlayerStatus.error:
-        return buildErrorIcon();
       default:
         return buildErrorIcon();
     }
