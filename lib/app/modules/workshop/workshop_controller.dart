77af60b062976ccf33b13871101d9b79843137be
Uriel <ubonano@gmail.com>
2020-10-18 23:36:18 -0300

    finish workshop page

--- lib/app/modules/workshop/workshop_controller.dart
+++ lib/app/modules/workshop/workshop_controller.dart
@@ -1,17 +1,21 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:habitat_ft_user/app/data/models/component_model.dart';
 import 'package:habitat_ft_user/app/data/repositories/workshop_repository.dart';
 
 class WorkshopController extends GetxController {
-  final WorkshopRepository repository = Get.find();
+  final WorkshopRepository _repository = Get.find();
 
   PageController pageController = PageController(initialPage: 0);
   RxList<Component> _components = <Component>[].obs;
+  RxBool _isFinished = false.obs;
 
   List<Component> get components => _components;
   String get workshopId => Get.arguments['workshopId'];
   int get currentPage => pageController.page.round();
+  bool get isFinished => _isFinished.value;
 
   @override
   void onInit() => fetchComponents();
@@ -29,7 +33,18 @@ class WorkshopController extends GetxController {
       );
 
   void fetchComponents() =>
-      repository.getAllComponents(workshopId).then(_setComponents);
+      _repository.getAllComponents(workshopId).then(_setComponents);
 
   void _setComponents(List<Component> value) => _components.value = value;
+
+  void finish() {
+    _isFinished.value = true;
+    // TODO CREAR REPOSITORY DE SUBSCRIPTION y hacer el complete aca!
+    Timer(Duration(seconds: 2), leave);
+  }
+
+  void leave() {
+    Get.delete<WorkshopController>();
+    Get.back();
+  }
 }
--- lib/app/modules/workshop/workshop_view.dart
+++ lib/app/modules/workshop/workshop_view.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 import 'package:habitat_ft_user/app/data/models/component_model.dart';
 import 'package:habitat_ft_user/app/modules/workshop/workshop_controller.dart';
 import 'package:habitat_ft_user/app/utils/build_widget.dart';
+import 'package:habitat_ft_user/app/utils/config/colors.dart';
+import 'package:habitat_ft_user/app/utils/config/styles.dart';
 
 import 'widgets/component/component_view.dart';
 import 'widgets/navigation_bar.dart';
@@ -10,9 +12,41 @@ import 'widgets/navigation_bar.dart';
 class WorkshopView extends GetView<WorkshopController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar,
-      body: body,
+    return Obx(
+      // TODO REFACTOR
+      () => controller.isFinished
+          ? Scaffold(
+              backgroundColor: CustomerColors.CELESTE_HABITAT,
+              body: Center(
+                child: Column(
+                  children: [
+                    Spacer(),
+                    Text(
+                      'Taller completado',
+                      style: CustomerStyles.TITULO_2_BLANCO,
+                    ),
+                    Icon(
+                      Icons.check_circle_outline,
+                      color: CustomerColors.BLANCO,
+                      size: 150,
+                    ),
+                    Spacer(),
+                    FlatButton(
+                      child: Text(
+                        'Salir',
+                        style: CustomerStyles.TITULO_3_BLANCO,
+                      ),
+                      onPressed: controller.leave,
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            )
+          : Scaffold(
+              appBar: appBar,
+              body: body,
+            ),
     );
   }
 
