f66191f39a036b43bc9e45b8d70a9b9a404f8092
Uriel <ubonano@gmail.com>
2020-10-24 17:55:40 -0300

    splash screen refactor

--- lib/app/modules/splash_screen/splash_screen_view.dart
+++ lib/app/modules/splash_screen/splash_screen_view.dart
@@ -3,19 +3,19 @@ import 'package:get/get.dart';
 import 'package:habitat_ft_user/app/utils/config/assets.dart';
 
 class SplashScreenView extends StatelessWidget {
+  final String asset;
+
+  const SplashScreenView({
+    this.asset = CustomerAssets.SPLASH_SCREEN,
+  });
+
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          child: Image.asset(
-            CustomerAssets.SPLASH_SCREEN,
-            fit: BoxFit.cover,
-            height: Get.height,
-            width: Get.width,
-          ),
-        )
-      ],
-    );
-  }
+  Widget build(BuildContext context) => Container(child: child);
+
+  Widget get child => Image.asset(
+        asset,
+        fit: BoxFit.cover,
+        height: Get.height,
+        width: Get.width,
+      );
 }
