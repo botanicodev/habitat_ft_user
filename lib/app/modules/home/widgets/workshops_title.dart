bcc01b4fecb3e2bbde284a00cc34348c6dfb9891
Uriel <ubonano@gmail.com>
2020-10-29 21:34:43 -0300

    refactor title

--- lib/app/modules/home/widgets/workshops_title.dart
+++ lib/app/modules/home/widgets/workshops_title.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:habitat_ft_user/app/utils/config/styles.dart';
 
-// TODO REFACTOR
 class WorkshopsTitle extends StatelessWidget {
-  const WorkshopsTitle();
+  final EdgeInsetsGeometry padding;
+
+  const WorkshopsTitle({
+    this.padding = const EdgeInsets.only(top: 40, bottom: 10),
+  });
+
+  String get title => 'Talleres';
+  TextStyle get style => CustomerStyles.TITULO_2_CELESTE_OSCURO;
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 40, bottom: 10),
-      child: Center(
-          child:
-              Text('Talleres', style: CustomerStyles.TITULO_2_CELESTE_OSCURO)),
-    );
-  }
+  Widget build(BuildContext context) => Padding(
+        padding: padding,
+        child: Center(child: Text(title, style: style)),
+      );
 }
